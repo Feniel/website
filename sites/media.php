d2d5212551af1037177f8e994be10659feb872a5
Feniel <Gamer_World@web.de>
2018-04-02 00:19:15 +0200

    Kommentare

--- sites/media.php
+++ sites/media.php
@@ -6,11 +6,13 @@
 <head>
     <meta charset="UTF-8">
     <title>LeNerds</title>
+    <!-- Bootstrap imports -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel= "stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap‐theme.min.css">
-    <link rel="stylesheet" type="text/css" href="../sheets/outer.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Custom Imports -->
+    <link rel="stylesheet" type="text/css" href="../sheets/outer.css"/>
     <link rel="icon" href="media/favicon.png"/>
 </head>
 <body>
