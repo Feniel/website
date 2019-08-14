aba382525e410f40e4340c7ab8bb4e99618a953d
timLaptop <tim.falkenhahn@gmail.com>
2019-08-14 11:58:36 +0200

    dropdown entfernt

--- includes/nav.php
+++ includes/nav.php
@@ -15,15 +15,7 @@
             <a href="../sites/datenbank.php" id="row">Datenbank</a>
         </div><div id="rowHolder"></div>
         <div id="logout">
-            <div class="dropdown">
-                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Account
-                    <span class="caret"></span></button>
-                <ul class="dropdown-menu">
-                    <li><a href="#">HTML</a></li>
-                    <li><a href="#">CSS</a></li>
-                    <li><a href="#">JavaScript</a></li>
-                </ul>
-            </div>
+            <a href="../includes/login/logout.php" id="row">Logout</a>
         </div>
     </div>
 </div>
